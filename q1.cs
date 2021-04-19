75eb7f83011f34c90a0936bb0e004bbcccdd05b4
yewyn <yewyn159gmail.com>
2021-04-19 17:07:36 +0800

    prog

--- q1.cs
+++ q1.cs
@@ -2,10 +2,21 @@ using System;
 
 public static class Program
 {
-    public static void Main(string[] args)
+    public static string letterCount(string str)
     {
-        
+        string[] words = str.Split(' ');
+        int longest = 0;
+        string longestWord ='';
 
+        for (int i = 0; i < str.length; i++)
+        {
+            string word = words[i];
+
+            for (int j = 0; j < word.length; j++)
+            {
+                if ()
+            }
+        }
 
     }
 }
