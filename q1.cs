c4074fde462dfafdfe95dc88022ac205253389ee
yewyn <yewyn159gmail.com>
2021-04-19 17:31:39 +0800

    latest

--- q1.cs
+++ q1.cs
@@ -1,21 +1,35 @@
 using System;
+using System.Text.RegularExpressions;
 
 public static class Program
 {
     public static string letterCount(string str)
     {
-        string[] words = str.Split(' ');
-        int longest = 0;
+        string[] delimitedWords = str.Split(' ');
+        int longest = 1;
         string longestWord ='';
+        int[] countObjects = {};
 
         for (int i = 0; i < str.length; i++)
         {
-            string word = words[i];
+            string word = delimitedWords[i];
 
             for (int j = 0; j < word.length; j++)
             {
-                if ()
+               if (countObjects[word[j]] == countObjects[word[j]] || 0)
+               {
+                   countObjects[word[j]]++;
+               }
             }
+        }
+
+        if longest == 1
+        {
+            return -1;
+        }
+        else
+        {
+            return longestWord;
         }
 
     }
