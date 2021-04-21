82bea331765674a0ea8f2efd59ce1764aae37837
yewyn <yewyn159gmail.com>
2021-04-21 14:20:30 +0800

    q2 updated

--- q2.cs
+++ q2.cs
@@ -8,32 +8,35 @@ public static class Program
 {
 	public static void Main(string[] arg)
 	{
-		string[] strArr = {"hellocat", "apple,bat,cat,goodbye,hello,yellow,why"};	
+		string[] strArr = {"hellocat", "apple,bat,cat,goodbye,hello,yellow,why"};
+		Console.WriteLine(WordSplit(strArr));
 	}
 	
     public static string WordSplit(string[] strArr)
     {
         string[] dic = strArr[1].Split(',');
 		List<string> dictionary = new List<string>();
-		
-		for (int i = 0; i < dic.Length; i++)
-		{
-			dictionary.Add(dic[i]);
-		}
-		
-		string[] characters = strArr[0].Split();
+		List<string> foundWords = new List<string>();
+	
+		dictionary.AddRange(dic);
+		char[] characters = strArr[0].ToCharArray();
 
         for (int i = 0; i < characters.Length; i++)
         {
 			// combine string from char 0 to i
-			string word = string.Join(characters.ToList().GetRange(0,i));
+			string word = new string(characters, 0, i);
 			// if combined string is in dictionary
-            if (dictionary.Contains())
+            if (dictionary.Contains(word))
 			{
 				// return words
-				return characters[0..i];
+				foundWords.Add(word);
 			}	
         }
+		
+		if (foundWords.Any())
+		{
+			return String.Join(", ", foundWords);
+		}
 		return "string not possible";
     }
 }
