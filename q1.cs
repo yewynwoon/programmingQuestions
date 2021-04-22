22994297f98502ed3800af65f398612a0c6d4311
yewyn <yewyn159gmail.com>
2021-04-22 17:27:17 +0800

    updated q1

--- q1.cs
+++ q1.cs
@@ -1,2 +1,43 @@
-word.GroupBy(w => w)
-                            .Max(grp => grp.Count());
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Test
+{
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string str = "Today is the greatest day ever";
+        Console.WriteLine(letterCount(str));
+    }
+    
+    public static string letterCount(string str)
+    {
+        var delimitedWords = new List<string>(str.Split(' '));
+        int longest = 1;
+        string longestWord = " ";
+		
+		foreach (var word in delimitedWords)
+		{	
+			var wordTuple = new Tuple<string,int>(word, word.GroupBy(w => w)
+                            .Max(grp => grp.Count()));
+                
+            if (wordTuple.Item2 > longest)
+            {
+                longest = wordTuple.Item2;
+                longestWord = wordTuple.Item1;
+            }
+		}
+		
+        if (longest == 1)
+        {
+            return "-1";
+        }
+        else
+        {
+            return longestWord;
+        }
+}
+}
+}
